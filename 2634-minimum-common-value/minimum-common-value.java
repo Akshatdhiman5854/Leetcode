d408c3f55be3f786c59d3c89b4427364a282c99b
Akshat Dhiman <94186209+Akshatdhiman5854@users.noreply.github.com>
2024-03-09 10:58:19 +0530

    Time: 1 ms (96.67%) | Memory: 62.4 MB (80.15%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,54 +1,74 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int i = 0, j=0;
 
-        for(int x: nums1) {
-            if(mp.containsKey(x)) {
-                continue;
+        while(i < n1 && j < n2) {
+            if(nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+            else if(nums1[i] > nums2[j]) {
+                j++;
             }
             else {
-                mp.put(x, 1);
+                i++;
             }
         }
 
-        for(int x: nums2) {
-            if(mp.containsKey(x)) {
-                mp.put(x, mp.getOrDefault(x, 0) + 1);
-            }
-            else {
-                continue;
-            }
-        }
-        int min = Integer.MAX_VALUE;
+        return -1;
+    }
+}
 
+//------------------
+//Using Hashmap
+// HashMap<Integer, Integer> mp = new HashMap<>();
 
-        Iterator<Integer> iterator = mp.values().iterator();
-        while (iterator.hasNext()) {
-            int x = iterator.next();
-            if (x < 2) {
-                iterator.remove();
-            }
-        }
-        System.out.println(mp);
-        if(mp.isEmpty()) {
-            return -1;
-        }
-        // for(int x: mp.values()) {
-        //     if(x != 2) {
-        //         mp.remove(x);
+        // for(int x: nums1) {
+        //     if(mp.containsKey(x)) {
+        //         continue;
+        //     }
+        //     else {
+        //         mp.put(x, 1);
         //     }
         // }
 
+        // for(int x: nums2) {
+        //     if(mp.containsKey(x)) {
+        //         mp.put(x, mp.getOrDefault(x, 0) + 1);
+        //     }
+        //     else {
+        //         continue;
+        //     }
+        // }
+        // int min = Integer.MAX_VALUE;
 
-        for(Map.Entry<Integer, Integer> itr: mp.entrySet()) {
-            int x = itr.getKey();
-            min = min < x? min: x;
-        }
 
-        // for(int x: mp.key()) {
+        // Iterator<Integer> iterator = mp.values().iterator();
+        // while (iterator.hasNext()) {
+        //     int x = iterator.next();
+        //     if (x < 2) {
+        //         iterator.remove();
+        //     }
+        // }
+        // System.out.println(mp);
+        // if(mp.isEmpty()) {
+        //     return -1;
+        // }
+        // // for(int x: mp.values()) {
+        // //     if(x != 2) {
+        // //         mp.remove(x);
+        // //     }
+        // // }
+
+
+        // for(Map.Entry<Integer, Integer> itr: mp.entrySet()) {
+        //     int x = itr.getKey();
         //     min = min < x? min: x;
         // }
 
-        return min;
-    }
-}
+        // // for(int x: mp.key()) {
+        // //     min = min < x? min: x;
+        // // }
+
+        // return min;

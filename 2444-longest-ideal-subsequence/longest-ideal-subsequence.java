abf587de980a7f6932dd02e0b00c29f97e7a40a0
Akshat Dhiman <94186209+Akshatdhiman5854@users.noreply.github.com>
2024-04-25 19:02:21 +0530

    Time: 13 ms (100.00%) | Memory: 44.7 MB (62.70%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.java
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.java
@@ -7,18 +7,16 @@ class Solution {
             char c = s.charAt(i);
             int idx = c - 'a';
             int max = Integer.MIN_VALUE;
-            int left = (idx - k) > 0? (idx - k): 0;//Math.max((idx - k), 0);
-            int right = (idx + k) < 26? (idx + k): 26;//Math.min((idx + k), 26);
+            int left = Math.max((idx - k), 0);
+            int right = Math.min((idx + k), 26);
             for (int j = left; j <= right; j++) {
-                // max = Math.max(max, dp[j]);
-                max = max > dp[j]? max: dp[j];
+                max = Math.max(max, dp[j]);
             }
             dp[idx] = max + 1;
         }
         int max = Integer.MIN_VALUE;
         for (int ele : dp) {
-            // max = Math.max(ele, max);
-            max = ele > max? ele: max;
+            max = Math.max(ele, max);
         }
         return max;
     }

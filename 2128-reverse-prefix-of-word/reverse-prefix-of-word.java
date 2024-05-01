5168053fede67b78aa5826c2e055a3b647e97cdd
Akshat Dhiman <94186209+Akshatdhiman5854@users.noreply.github.com>
2024-05-01 18:18:52 +0530

    Time: 1 ms (49.93%) | Memory: 41.5 MB (56.56%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -1,32 +1,39 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        int n = word.length();
-        int indx = -1;
-        int i;
-        int flag = 0;
-        for(i=0; i<n; i++) {
-            if(word.charAt(i) == ch) {
-                indx = i;
-                i++;
-                flag = 1;
-                break;
-            }
-        }
-
-        if(flag == 0) {
+        int firstOccurence = word.indexOf(ch);
+        if(firstOccurence == -1){
             return word;
         }
-        String ss = "";
+        
+        StringBuilder prefix = new StringBuilder(word.substring(0, firstOccurence+1));
+        return prefix.reverse().toString() + word.substring(firstOccurence+1);
+        // int n = word.length();
+        // int indx = -1;
+        // int i;
+        // int flag = 0;
+        // for(i=0; i<n; i++) {
+        //     if(word.charAt(i) == ch) {
+        //         indx = i;
+        //         i++;
+        //         flag = 1;
+        //         break;
+        //     }
+        // }
 
-        while(indx>=0) {
-            ss += word.charAt(indx);
-            indx--;
-        }
-        while(i < n) {
-            ss += word.charAt(i);
-            i++;
-        }
+        // if(flag == 0) {
+        //     return word;
+        // }
+        // String ss = "";
 
-        return ss;
+        // while(indx>=0) {
+        //     ss += word.charAt(indx);
+        //     indx--;
+        // }
+        // while(i < n) {
+        //     ss += word.charAt(i);
+        //     i++;
+        // }
+
+        // return ss;
     }
 }

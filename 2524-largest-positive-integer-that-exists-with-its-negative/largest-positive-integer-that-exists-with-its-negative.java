67150bbc033b2f2a06d699f97fe906c568657a98
Akshat Dhiman <94186209+Akshatdhiman5854@users.noreply.github.com>
2024-05-03 00:45:11 +0530

    Time: 7 ms (35.86%) | Memory: 44.4 MB (95.48%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -3,21 +3,38 @@ class Solution {
         if(nums.length == 1) {
             return -1;
         }
-        Arrays.sort(nums);
-        int n = nums.length;
-        int i = 0;
-        int j = n-1;
-        
-        while(i < j) {
-            if((-1*nums[i]) == nums[j]) {
-                return nums[j];
-            } else if((-1*nums[i]) > nums[j]) {
-                i++;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i< nums.length; i++) {
+            if(mp.containsKey(-1*nums[i])) {
+                max = nums[i] > max? nums[i] : max;
+                max = (-1*nums[i]) > max? (-1*nums[i]) : max;
+                mp.put(nums[i], mp.getOrDefault(nums[i], 1));
             } else {
-                j--;
+                mp.put(nums[i], 1);
             }
         }
 
-        return -1;
+        if(max == Integer.MIN_VALUE) {
+            return -1;
+        }
+        return max;
+
+        // Arrays.sort(nums);
+        // int n = nums.length;
+        // int i = 0;
+        // int j = n-1;
+        
+        // while(i < j) {
+        //     if((-1*nums[i]) == nums[j]) {
+        //         return nums[j];
+        //     } else if((-1*nums[i]) > nums[j]) {
+        //         i++;
+        //     } else {
+        //         j--;
+        //     }
+        // }
+        // return -1;
     }
 }

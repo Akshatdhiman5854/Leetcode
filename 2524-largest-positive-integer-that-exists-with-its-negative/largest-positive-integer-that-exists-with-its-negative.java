0353f2542d748c5aa611f075938ad950ba8b74f3
Akshat Dhiman <94186209+Akshatdhiman5854@users.noreply.github.com>
2024-05-03 00:47:57 +0530

    Time: 5 ms (84.11%) | Memory: 44.5 MB (83.24%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -4,37 +4,38 @@ class Solution {
             return -1;
         }
 
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i< nums.length; i++) {
-            if(mp.containsKey(-1*nums[i])) {
-                max = nums[i] > max? nums[i] : max;
-                max = (-1*nums[i]) > max? (-1*nums[i]) : max;
-                mp.put(nums[i], mp.getOrDefault(nums[i], 1));
+        Arrays.sort(nums);
+        int n = nums.length;
+        int i = 0;
+        int j = n-1;
+        
+        while(i < j) {
+            if((-1*nums[i]) == nums[j]) {
+                return nums[j];
+            } else if((-1*nums[i]) > nums[j]) {
+                i++;
             } else {
-                mp.put(nums[i], 1);
+                j--;
             }
         }
+        return -1;
 
-        if(max == Integer.MIN_VALUE) {
-            return -1;
-        }
-        return max;
 
-        // Arrays.sort(nums);
-        // int n = nums.length;
-        // int i = 0;
-        // int j = n-1;
-        
-        // while(i < j) {
-        //     if((-1*nums[i]) == nums[j]) {
-        //         return nums[j];
-        //     } else if((-1*nums[i]) > nums[j]) {
-        //         i++;
+        // HashMap<Integer, Integer> mp = new HashMap<>();
+        // int max = Integer.MIN_VALUE;
+        // for(int i = 0; i< nums.length; i++) {
+        //     if(mp.containsKey(-1*nums[i])) {
+        //         max = nums[i] > max? nums[i] : max;
+        //         max = (-1*nums[i]) > max? (-1*nums[i]) : max;
+        //         mp.put(nums[i], mp.getOrDefault(nums[i], 1));
         //     } else {
-        //         j--;
+        //         mp.put(nums[i], 1);
         //     }
         // }
-        // return -1;
+
+        // if(max == Integer.MIN_VALUE) {
+        //     return -1;
+        // }
+        // return max;
     }
 }

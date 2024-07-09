3aff8d9a5e5dc43b29a2a6f5b00c55c23c146f9c
Akshat Dhiman <94186209+Akshatdhiman5854@users.noreply.github.com>
2024-07-09 17:23:22 +0530

    Time: 3 ms (93.06%) | Memory: 88.9 MB (5.31%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -4,25 +4,18 @@ class Solution {
 
         int ti = customers[0][0];
         int n = customers.length;
+        double ans = 0;
 
         for(int i = 0; i < n; i++) {
             if(ti < customers[i][0]) {
                 ti = customers[i][0];
             }
             ti = ti + customers[i][1];
-            wait[i] = ti - customers[i][0];
-        }
-
-        double ans = 0;
-        for(int i = 0; i < n; i++) {
-            ans += wait[i];
-            System.out.print(wait[i]+" ");
+            ans = ans + ti - customers[i][0];
         }
 
         ans = ans / n;
 
         return ans;
-
-
     }
 }

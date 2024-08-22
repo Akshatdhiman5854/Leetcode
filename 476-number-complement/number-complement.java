640ab780881573dd9a18e891bf84b32b2a8697c5
Akshat Dhiman <94186209+Akshatdhiman5854@users.noreply.github.com>
2024-08-22 20:36:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (20.00%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,15 +1,29 @@
 class Solution {
     public int findComplement(int num) {
-        String str = Integer.toBinaryString(num);
-        String res = "";
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i) == '0'){
-                res += '1';
-            }
-            else{
-                res += '0';
-            }
-        }
-        return Integer.parseInt(res,2);
+        if (num == 0) return 1;
+
+        int bitLength = Integer.toBinaryString(num).length();
+        
+        int mask = (1 << bitLength) - 1;
+        
+        return num ^ mask;
     }
 }
+
+
+
+// class Solution {
+//     public int findComplement(int num) {
+//         String str = Integer.toBinaryString(num);
+//         String res = "";
+//         for(int i=0;i<str.length();i++){
+//             if(str.charAt(i) == '0'){
+//                 res += '1';
+//             }
+//             else{
+//                 res += '0';
+//             }
+//         }
+//         return Integer.parseInt(res,2);
+//     }
+// }
